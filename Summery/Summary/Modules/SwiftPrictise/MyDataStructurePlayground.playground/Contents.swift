eee52065752324dca76f7db7d7872b89a3644694
lugang <anothertree277@163.com>
2020-01-07 19:26:10 +0800

    swift 练习

--- Summery/Summary/Modules/SwiftPrictise/MyDataStructurePlayground.playground/Contents.swift
+++ Summery/Summary/Modules/SwiftPrictise/MyDataStructurePlayground.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
-
+let str1 = "Hello world"
 var str = "Hello, playground"
+var num = 100
+str = "hello Gang"
+//来个if判断语句吧
+
+if num == 100 {
+    print(num)
+}
+//print(piao \(str1))
+
+
+
