91097faced352f03b60f00d4dfd84b0fb77c60ad
lugang <anothertree277@163.com>
2020-01-07 19:05:03 +0800

    pod 不在版本控制里边

--- Summery/Pods/Target Support Files/Pods-SummaryTests/Pods-SummaryTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SummaryTests : NSObject
-@end
-@implementation PodsDummy_Pods_SummaryTests
-@end
--- Summery/Pods/Target Support Files/Pods-SummaryUITests/Pods-SummaryUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SummaryUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SummaryUITests
-@end
